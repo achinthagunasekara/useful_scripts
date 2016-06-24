22ab98ce12c4bbd7238d2e5b4c3ec8d0fd33df1e
Achintha Gunasekara <achintha@me.com>
2016-06-24 16:21:10 +1000

    Update and rename update_all.sh to git_update_all_repos.sh

--- git_update_all_repos.sh
+++ git_update_all_repos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+#script to run git pull for all repos in current directory
 for D in `find . -type d -maxdepth 1 -not -path '*/\.*' -not -name '.'`
 do
   printf "  \e[31m%-5s\e[m%s\n" "Git Pulling on" "${D}" # red
